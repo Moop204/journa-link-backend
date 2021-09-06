979494083982672158f5f12ac60d18bcbafe3ed3
Moop204 <andrew.nguyen2045@outlook.com>
2021-09-07 05:41:18 +1000

    Heroku

---
+++ index.ts
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+
+// Uses Heroku defined port number or else 3000
+const port = process.env.PORT || 3000;
+
+app.get("/api", (req: any, res: any) => {
+  res.status(200).json({ api: "version 1" });
+});
+
+app.get("/", (req: any, res: any) => {
+  res.status(200).json({ base: "other" });
+});
+
+app.listen(3000, () => console.log("server started"));
