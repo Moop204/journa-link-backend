7e24162e7d69fd0750f140a39715c3ac614a11d8
Moop204 <andrew.nguyen2045@outlook.com>
2021-09-09 09:51:50 +1000

    [feat] (db/set-database.ts) Aggregates json files from news-please to initialise db

---
+++ db/process-scraped.ts
@@ -0,0 +1,91 @@
+import path from "path";
+import fs from "fs";
+
+const directoryPath = path.join(
+  __dirname,
+  "../../../../../news-please-repo/data/2021/09/07/nytimes.com"
+);
+
+const aggregateFiles = () => {
+  const res: JSON[] = [];
+  const files = fs.readdirSync(directoryPath);
+
+  console.log("\nCurrent directory filenames:");
+  files.forEach((file) => {
+    if (file.match(/.*\.json/)) {
+      const str = fs.readFileSync(directoryPath + "/" + file, "utf8");
+      const instance = JSON.parse(str);
+      delete instance["image_url"];
+      delete instance["description"];
+      delete instance["date_download"];
+      delete instance["date_modify"];
+      delete instance["date_publish"];
+      delete instance["filename"];
+      delete instance["maintext"];
+      delete instance["language"];
+      delete instance["localpath"];
+      delete instance["title_page"];
+      delete instance["title_rss"];
+      delete instance["description"];
+
+      if (instance.authors.length > 0) {
+        instance.authors = instance.authors.map((name: string) => {
+          // Manual changes depending on what you notice
+          name = name.replace(" Special To The New York Times", "");
+          name = name.replace(/\./g, "");
+          name = name.trim();
+          // console.log(name);
+          return name;
+        });
+        res.push(instance);
+      }
+    }
+  });
+  return res;
+};
+
+interface ArticleDescriptor extends JSON {
+  authors: string[];
+  title: string;
+  source_domain: string;
+  url: string;
+}
+
+const authorProfile = (articles: ArticleDescriptor[]) => {
+  const authorDict: any = {};
+  articles.forEach((article) => {
+    const articleDesc = {
+      title: article.title,
+      link: article.url,
+    };
+    article.authors.forEach((author) => {
+      if (authorDict[author]) {
+        if (authorDict[author][article.source_domain]) {
+          // console.log("Should reach");
+          authorDict[author][article.source_domain].push(articleDesc);
+        } else {
+          // console.log("No reach");
+          authorDict[author][article.source_domain] = [articleDesc];
+        }
+      } else {
+        // console.log("Definitely");
+        authorDict[author] = {};
+        authorDict[author][article.source_domain] = [articleDesc];
+      }
+    });
+  });
+  return authorDict;
+};
+
+const obtainDataStructure = () => {
+  const here = aggregateFiles();
+  // console.log(here);
+  return authorProfile(here as ArticleDescriptor[]);
+  // const res = fs.writeFileSync(
+  //   __dirname + "/out.json",
+  //   '{"res":' + JSON.stringify(here) + "}",
+  //   {}
+  // );
+};
+
+export { obtainDataStructure };
--- db/set-database.ts
+++ db/set-database.ts
@@ -1,5 +1,7 @@
+import { Sequelize, Op, DataTypes } from "sequelize";
+import { obtainDataStructure } from "./process-scraped";
+
 const dotenv = require("dotenv");
-const { Sequelize } = require("sequelize");
 
 const sequelize = new Sequelize(process.env.CONNECTION_STRING, {
   dialectOptions: {
@@ -10,25 +12,166 @@ const sequelize = new Sequelize(process.env.CONNECTION_STRING, {
   },
 });
 
-// const sequelize = new Sequelize(database, user, password, {
-//   host: host,
-//   dialect: 'postgres',
-//   dialectOptions: {
-//     ssl: {
-//       key: cKey,
-//       cert: cCert,
-//       ca: cCA
-//     }
-//   }
-// });
+const Reporter = sequelize.define(
+  "Reporter",
+  {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    work: {
+      type: DataTypes.JSON,
+      allowNull: false,
+    },
+  },
+  { freezeTableName: true }
+);
 
-const test = async () => {
-  try {
-    await sequelize.authenticate();
-    console.log("Connection has been established successfully.");
-  } catch (error) {
-    console.error("Unable to connect to the database:", error);
-  }
+const Publisher = sequelize.define(
+  "Publisher",
+  {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    link: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  { freezeTableName: true }
+);
+
+const WorkFor = sequelize.define(
+  "WorkFor",
+  {
+    reporter: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "Reporter",
+        key: "id",
+      },
+    },
+    publisher: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "Publisher",
+        key: "id",
+      },
+    },
+  },
+  { freezeTableName: true }
+);
+
+// const Article = sequelize.define(
+//   "Article",
+//   {
+//     title: {
+//       type: DataTypes.STRING,
+//       allowNull: false,
+//     },
+//     link: {
+//       type: DataTypes.STRING,
+//       allowNull: false,
+//     },
+//     publisher: {
+//       type: DataTypes.INTEGER,
+//       allowNull: false,
+//       references: {
+//         model: "Publisher",
+//         key: "id",
+//       },
+//     },
+//   },
+//   { freezeTableName: true }
+// );
+
+// const ReportedOn = sequelize.define(
+//   "ReportedOn",
+//   {
+//     reporter: {
+//       type: DataTypes.INTEGER,
+//       allowNull: false,
+//       references: {
+//         model: "Reporter",
+//         key: "id",
+//       },
+//     },
+//     article: {
+//       type: DataTypes.INTEGER,
+//       allowNull: false,
+//       references: {
+//         model: "Article",
+//         key: "id",
+//       },
+//     },
+//   },
+//   { freezeTableName: true }
+// );
+
+// // const test = async () => {
+// //   try {
+// //     await sequelize.authenticate();
+// //     console.log("Connection has been established successfully.");
+// //   } catch (error) {
+// //     console.error("Unable to connect to the database:", error);
+// //   }
+// // };
+
+// // test();
+
+const query = async () => {
+  console.log(
+    await (await sequelize.models.Reporter.findOne()).getDataValue("work")
+  );
 };
 
-test();
+query();
+
+const simpleInsert = async () => {
+  const data = obtainDataStructure();
+
+  await sequelize.sync({ force: true });
+  await sequelize.models.Publisher.create({
+    name: "nytimes",
+    link: "nytimes.com",
+  });
+
+  const publisherCache: { [index: string]: number } = {};
+
+  Object.entries(data).forEach(async ([author, profile]) => {
+    const articlesWritten = Object.values(profile).reduce(
+      (articles: JSON[], curArticles: JSON[]) => {
+        articles.concat(curArticles);
+      }
+    );
+    const reporter = await sequelize.models.Reporter.create({
+      name: author,
+      work: articlesWritten,
+    });
+    const reporterId = reporter.getDataValue("id");
+
+    Object.entries(profile).forEach(async ([publisher, articles]) => {
+      if (!publisherCache[publisher]) {
+        const publisherRecord = await sequelize.models.Publisher.findOne({
+          where: {
+            link: publisher,
+          },
+        });
+        publisherCache[publisher] = publisherRecord.getDataValue("id");
+      }
+      await sequelize.models.WorkFor.create({
+        reporter: reporterId,
+        publisher: publisherCache[publisher],
+      });
+    });
+  });
+
+  // await sequelize.models.Reporter.create({ name: "another name" });
+  // const users = await sequelize.models.Reporter.findAll();
+  // console.log(users);
+};
+
+// simpleInsert();
